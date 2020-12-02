f7a8b8c05e8770c4805f1aaa05bf467fa316298b
TudorDan <tudor.ist@gmail.com>
2020-12-02 14:11:15 +0200

    Move test to test file

--- tests.py
+++ tests.py
@@ -6,6 +6,11 @@ from ui import *
 
 class CalculatorTestCase(unittest.TestCase):
 
+    # Test function
+    def test_multiply(self):
+		result = calculate_multiplication(3, 5)
+        self.assertion(15, result)
+
     def test_calculate_subtraction_success(self):
         result = calculate_subtraction(10,5,1)
         self.assertEqual(4, result)
